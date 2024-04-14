d98c0143445e25d7ade17f133e0400d529cefd91
spalnatik <139609488+spalnatik@users.noreply.github.com>
2024-04-14 11:41:39 +0530

    Update shareddisk2.sh

--- shareddisk2.sh
+++ shareddisk2.sh
@@ -3,3 +3,7 @@
 disk=`ls -l /dev/disk/by-id/scsi-* | grep sdc | awk 'NR==2 {print $9}'`
 
 sed -i "s|^SBD_DEVICE=.*|SBD_DEVICE=\"$disk\"|" /etc/sysconfig/sbd
+
+echo softdog | sudo tee /etc/modules-load.d/softdog.conf
+
+sudo modprobe -v softdog
