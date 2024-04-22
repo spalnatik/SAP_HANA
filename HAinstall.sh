1ffc2cea06b186e57c17bba2e4e9b179bc961202
spalnatik <139609488+spalnatik@users.noreply.github.com>
2024-04-22 08:45:19 +0530

    Update HAinstall.sh

--- HAinstall.sh
+++ HAinstall.sh
@@ -31,7 +31,7 @@ zypper --non-interactive --no-refresh install libtool
 
 chmod +x /HDB_SERVER_LINUX_X86_64/instruntime/sdbrun 
 
-./hdblcm --ignore=check_signature_file
+#./hdblcm --ignore=check_signature_file
 
 printf "\n1\n\n\n\nHN1\n03\n\n2\n\n\n\n\n\nAbc@12345678\nAbc@12345678\nAbc@12345678\nAbc@12345678\n\n\n\n\nAbc@12345678\nAbc@12345678\n\ny" | ./hdblcm --ignore=check_signature_file
 
