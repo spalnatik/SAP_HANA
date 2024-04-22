8dbeab050939cdceb7155718bc5e8bcda17b59b4
spalnatik <139609488+spalnatik@users.noreply.github.com>
2024-04-22 09:09:43 +0530

    Create hanasyncsec.sh

---
+++ hanasyncsec.sh
@@ -0,0 +1,37 @@
+su - hn1adm
+cdtrace
+sapcontrol -nr 00 -function StopWait 600 10
+hdbnsutil -sr_register --remoteHost=hn1-db-0 --remoteInstance=00 --replicationMode=sync --name=SITE2
+## check the status 
+hdbnsutil -sr_state
+
+sapcontrol -nr 00 -function StopSystem
+exit
+
+lines_to_add="[ha_dr_provider_SAPHanaSR]
+provider = SAPHanaSR
+path = /usr/share/SAPHanaSR
+execution_order = 1
+ 
+[ha_dr_provider_suschksrv]
+provider = susChkSrv
+path = /usr/share/SAPHanaSR
+execution_order = 3
+action_on_lost = fence
+ 
+[trace]
+ha_dr_saphanasr = info"
+
+# File path to the global file
+global="/hana/shared/HN1/global/hdb/custom/config/global.ini"
+
+echo "$lines_to_add" | sudo tee -a "$global"
+ 
+cat << EOF > /etc/sudoers.d/20-saphana
+> # Needed for SAPHanaSR and susChkSrv Python hooks
+> hn1adm ALL=(ALL) NOPASSWD: /usr/sbin/crm_attribute -n hana_hn1_site_srHook_*
+> hn1adm ALL=(ALL) NOPASSWD: /usr/sbin/SAPHanaSR-hookHelper --sid=HN1 --case=fenceMe
+> EOF
+ 
+su - hn1adm
+sapcontrol -nr 00 -function StartSystem
