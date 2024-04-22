735c02cf868e4e78e93de2c06277b1fe87e8d2d4
spalnatik <139609488+spalnatik@users.noreply.github.com>
2024-04-22 11:10:35 +0530

    Update hanasyncsec.sh

--- hanasyncsec.sh
+++ hanasyncsec.sh
@@ -1,5 +1,4 @@
-su - hn1adm -c 'cdtrace && \
-sapcontrol -nr 00 -function StopWait 600 10 && \
+su - hn1adm -c 'sapcontrol -nr 00 -function StopWait 600 10 && \
 hdbnsutil -sr_register --remoteHost=hn1-db-0 --remoteInstance=00 --replicationMode=sync --name=SITE2 '
 ## check the status 
 hdbnsutil -sr_state
