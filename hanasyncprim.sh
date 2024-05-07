954e6000ebaecb57f3e23a93e0d848de454f6a65
spalnatik <139609488+spalnatik@users.noreply.github.com>
2024-05-07 08:37:24 +0530

    Update hanasyncprim.sh

--- hanasyncprim.sh
+++ hanasyncprim.sh
@@ -7,7 +7,7 @@ zypper --non-interactive --no-refresh install SAPHanaSR
 cat << EOF > /drsync.sh
 su - hn1adm -c 'HDB start && \
 hdbsql -d SYSTEMDB -u SYSTEM -p "Abc@12345678" -i 03 "BACKUP DATA USING FILE ('\'initialbackupSYS\'')" && \
-hdbsql -d HN1 -u SYSTEM -p "Abc@12345678" -i 03 "BACKUP DATA USING FILE ('\'initialbackupHN1\'')"
+hdbsql -d HN1 -u SYSTEM -p "Abc@12345678" -i 03 "BACKUP DATA USING FILE ('\'initialbackupHN1\'')"'
 
 EOF
 
