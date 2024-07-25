1cdb89ae185e14f7b31223367e7dbe98d28690ee
spalnatik <139609488+spalnatik@users.noreply.github.com>
2024-07-25 11:57:48 +0530

    Update susemi.sh

--- susemi.sh
+++ susemi.sh
@@ -4,7 +4,7 @@ subscriptionID=$2
 
 sudo crm configure primitive rsc_st_azure stonith:fence_azure_arm \
 params msi=true subscriptionId="$subscriptionID" resourceGroup="$rgname" \
-pcmk_monitor_retries=4 pcmk_action_limit=3 power_timeout=240 pcmk_reboot_timeout=900 pcmk_delay_max=15 " \
+pcmk_monitor_retries=4 pcmk_action_limit=3 power_timeout=240 pcmk_reboot_timeout=900 pcmk_delay_max=15  \
 op monitor interval=3600 timeout=120
 
 sudo crm configure property stonith-timeout=900
