5859f22297d65ed0f96e6f6d4da3cc9997fa40d8
spalnatik <139609488+spalnatik@users.noreply.github.com>
2024-08-08 10:39:04 +0530

    Update HAinstall.sh

--- HAinstall.sh
+++ HAinstall.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-sudo mkdir /mnt/hanashare
-if [ ! -d "/etc/smbcredentials" ]; then
-sudo mkdir /etc/smbcredentials
-fi
-if [ ! -f "/etc/smbcredentials/fuseshare25.cred" ]; then
-    sudo bash -c 'echo "username=fuseshare25" >> /etc/smbcredentials/fuseshare25.cred'
-    sudo bash -c 'echo "password=E5y36dFvqYToy2hwXd2L6T9lzVjNalpYG/1YQKtgxWMFFIrimcttKbtDtCbtxNBOoBP2sk5qQxO++AStCj8//A==" >> /etc/smbcredentials/fuseshare25.cred'
-fi
+
 sudo chmod 600 /etc/smbcredentials/fuseshare25.cred
 
 sudo bash -c 'echo "//fuseshare25.file.core.windows.net/hanashare /mnt/hanashare cifs nofail,credentials=/etc/smbcredentials/fuseshare25.cred,dir_mode=0777,file_mode=0777,serverino,nosharesock,actimeo=30" >> /etc/fstab'
